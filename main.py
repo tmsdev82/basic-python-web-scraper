aebae210152b7c4b462d2e8d060ea03a8ecc547b
Tim Steeman <tmdev82@gmail.com>
2024-02-11 11:03:32 +0100

    implemented extracting and printing h1 tag data

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
 import sys
+from bs4 import BeautifulSoup
 
 result = requests.get(sys.argv[1])
 
-print(result.text)
+soup = BeautifulSoup(result.text, 'html.parser')
+
+h1_tags = soup.find_all('h1')
+
+for tag in h1_tags:
+    print(tag.get_text())
+
 print(sys.argv)
