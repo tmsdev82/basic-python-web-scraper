f0fec0fbee7c72a10b218b20e53fc16af98bb38a
Tim Steeman <tmdev82@gmail.com>
2024-02-18 16:13:35 +0100

    added getting table information

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ h1_tags = soup.find_all('h1')
 
 for tag in h1_tags:
     print(tag.get_text())
+    
+tables = soup.find_all('table')
+
+print(f"number of tables: {len(tables)}")
+
+for index, table in enumerate(tables):
+    print(f"{index}: {table.get_text()}")
+    print("-----")
 
 print(sys.argv)
